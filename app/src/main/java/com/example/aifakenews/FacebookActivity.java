81571888a3b6f6c4b5f8856707c46a3221761be7
aedwards4 <aedwards4@hawk.iit.edu>
2021-03-08 23:16:35 -0600

    Updated Facebook Login user story

--- app/src/main/java/com/example/aifakenews/FacebookActivity.java
+++ app/src/main/java/com/example/aifakenews/FacebookActivity.java
@@ -5,19 +5,32 @@ import android.util.Log;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FacebookActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
     private LoginButton loginButton;
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,8 @@ public class FacebookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_facebook);
 
         loginButton = findViewById(R.id.login_button);
+        imageView = findViewById(R.id.profilePic);
+        textView = findViewById(R.id.nameText);
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -54,5 +69,44 @@ public class FacebookActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            String name = object.getString("name");
+                            String id = object.getString("id");
+                            textView.setText(name);
+                            Picasso.get().load("https://graph.facebook.com/" + id + "/picture/type-large")
+                                    .into(imageView);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fields", "gender, name, id, first_name, last_name");
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
+    }
+
+    AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null) {
+                LoginManager.getInstance().logOut();
+                textView.setText("");
+                imageView.setImageResource(0);
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
     }
 }
