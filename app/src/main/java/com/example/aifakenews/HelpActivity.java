7fa150502fc1a5c579ccd4bd75052ffb0d27db73
GeorgeTapia <gtapia.2400@gmail.com>
2021-03-30 19:00:19 -0500

    Scroll Bar with how to info

--- app/src/main/java/com/example/aifakenews/HelpActivity.java
+++ app/src/main/java/com/example/aifakenews/HelpActivity.java
@@ -15,17 +15,16 @@ public class HelpActivity extends AppCompatActivity {
         TextView mTitleWindow = (TextView) findViewById(R.id.titleWindow);
         TextView mMessageWindow = (TextView) findViewById(R.id.messageWindow);
 
-        mTitleWindow.setText("(AI FakeNews)\nThis is an app that uses an AI model to detect if a link contains fake news\nHow to use application:\n");
+        mTitleWindow.setText("(AI FakeNews)\n\nThis is an app that uses an AI model to detect if a link contains fake news\n\nHow to use application:\n\n");
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        String someMessage = " Having trouble using the app? No problem! we are here to help!\n " +
-                "I'm sure you're aware already! the main purpose of our app is to fight fake news! that being said, we implemented a couple features to help with this. Below, we will go over our app and give you an overview on how to use it.\n"
-                +"At the main menu, you have a search bar at the top, with three buttons below. Lets go over these:\n"
-                +"(Search Bar)- the search bar takes the input of an article URL that you would like our system to determine if it comes from a credible source or not.\n"
-                +"(Facebook/Twitter Buttons)- clicking on these buttons, redirects you to a different page where you can choose to log in to these platforms through our app. Simply, click \"continue to the site\" and enter your login credentials. Our AI will help fight fake news while you are surfing through Facebook/Twitter!"
-                +"(Home Button)- last of all, the home button takes you back to the main page."
-                +"©2021, Franklin Lu, Alexis Edwards, George Tapia, Truong Pham";
+        String someMessage = " Having trouble using the app? No problem! we are here to help!\n\n " +
+                "I'm sure you're aware already! the main purpose of our app is to fight fake news! that being said, we implemented a couple features to help with this. Below, we will go over our app and give you an overview on how to use it.\n\n"
+                +"At the main menu, you have a search bar at the top, with three buttons below. Lets go over these:\n\n"
+                +"(Search Bar)- the search bar takes the input of an article URL that you would like our system to determine if it comes from a credible source or not.\n\n"
+                + "(Home Button)- last of all, the home button takes you back to the main page."
+                +"\n\n©2021, Franklin Lu, Alexis Edwards, George Tapia, Truong Pham";
 
         stringBuilder.append(someMessage);
 
